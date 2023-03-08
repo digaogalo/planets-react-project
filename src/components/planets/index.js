1fff4a0db1b99120ed343c209236114901569923
digaogalo <digaolimac@gmail.com>
2023-03-08 17:42:05 -0300

    hooks useEffect useState

--- src/components/planets/index.js
+++ src/components/planets/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import Planet from './planet'
 
 async function getPlanets() {
@@ -7,60 +7,46 @@ async function getPlanets() {
     return data
 }
 
-class Planets extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            planets: []
-        }
-    }
+const Planets = () => {
+    const [planets, setPlanets] = useState([])
 
-        componentDidMount(){
-            getPlanets().then(data => {
-                this.setState(state => ({
-                    planets: data['planets']
-                }))
-            })
-         } 
-        
+    useEffect(() => {
+       getPlanets().then(data => {
+        setPlanets(data['planets'])
+     })
+    }, [])
 
-
-      removeLast =  () => {
-        let new_planets = [...this.state.planets]
+      const removeLast =  () => {
+        let new_planets = [...planets]
         new_planets.pop()
-        this.setState(state => ({
-            planets: new_planets
-        }))
+        setPlanets(new_planets)
     }
 
-    duplicateLastPlanet = () => {
-        let last_planet= this.state.planets[this.state.planets.length - 1]
-        this.setState(state => ({
-            planets: [...this.state.planets, last_planet]
-        }))
+        const duplicateLastPlanet = () => {
+        let last_planet= planets[planets.length - 1]
+        setPlanets([...planets, last_planet])
     }
 
-    render() {
         return (
-            <Fragment>
-                <h3>Planets List</h3>
-                <button onClick={this.removeLast}>Remove Last</button>
-                <button onClick={this.duplicateLastPlanet}>Duplicate Last</button>
-                <hr/>
-                {this.state.planets.map((planet, index) =>
-                <Planet  
-                id={planet.id}   
+          <Fragment>
+            <h3>Planets List</h3>
+            <button onClick={removeLast}>Remove Last</button>
+            <button onClick={duplicateLastPlanet}>Duplicate Last</button>
+            <hr />
+            {planets.map((planet, index) => (
+              <Planet
+                id={planet.id}
                 name={planet.name}
                 description={planet.description}
                 img_url={planet.img_url}
                 link={planet.link}
                 key={index}
-                />  
-                )}             
-            </Fragment>
-        )
+              />
+            ))}
+          </Fragment>
+        );
     }
- }
+
 
 
 
--- src/components/planets/planet/index.js
+++ src/components/planets/planet/index.js
@@ -1,54 +1,49 @@
 
-import React from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import GrayImg from '../../shared/gray_img' 
 import DescriptionWithLink from '../../shared/description_with_link/index'
 
 async function getSatellites(id) {
-    let response = await fetch ("http://localhost:3000/api/satellites.json")
+    let response = await fetch (`http://localhost:3000/api/${id}.json`)
     let data = await response.json()
     return data
 }
 
-class Planet extends React.Component {
+
+
+const Planet = (props) => {
+  const [satellites, setSatellites] = useState([])
+
+      useEffect(() => {
+        getSatellites(props.id).then(data => {
+        setSatellites(data['satellites'])
+          
+        }) 
+    },[])
+
     
-    constructor(props) {
-        super(props)
-        this.state = {
-            satellites: []
-        }
-    }
-
-    componentDidMount(){
-        getSatellites(this.props.id).then(data => {
-            this.setState(state =>({
-                satellites: data['satellites']
-            }))
-        })
-    }
-
-    render() {
-        let title
-        if(this.props.title_with_underline) 
-        title = <h4><u>{this.props.name}</u></h4>
+ 
+         let title
+        if(props.title_with_underline) 
+        title = <h4><u>{props.name}</u></h4>
         else
-        title = <h4>{this.props.name}</h4>
-        return (
+        title = <h4>{props.name}</h4>
 
+        return (
         <div>
             {title}
-            <DescriptionWithLink description={this.props.description} link={this.props.link}/>
-            <GrayImg img_url={this.props.img_url} gray={this.props.gray}/>
+            <DescriptionWithLink description={props.description} link={props.link}/>
+            <GrayImg img_url={props.img_url} gray={props.gray}/>
             <h4>Satélites</h4>
             <ul>
-                {this.state.satellites.map((satellites, index )=> 
-                <li key={index}>{satellites.name}s</li>
+                {satellites.map((satellites, index )=> 
+                <li key={index}>{satellites.name}</li>
                 )}
             </ul>
             
             <hr/>
         </div>           
-    )
-  }
+    ) 
 }
 
 
